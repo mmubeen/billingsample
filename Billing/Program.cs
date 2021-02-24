06fa804666cd9572844ea00d8052e1762227bf3f
cn202605 <mrezam@hotmail.com>
2021-02-23 23:25:19 -0600

    refactoring

--- Billing/Program.cs
+++ Billing/Program.cs
@@ -6,7 +6,33 @@ namespace Billing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var posOnline = new Pos();
+            var input = "Y";
+            do
+            {
+                Console.WriteLine("Enter 1,2,3,4, B to Print Bill or N to stop billing or R to clear order.");
+                input = Console.ReadLine().ToUpper();
+                switch (input)
+                {
+                    case "B":
+                        Console.Write(posOnline.displayBill());
+                        break;
+                    case "R":
+                        posOnline.reset();
+                        Console.Write(posOnline.displayBill());
+                        break;
+                    default:
+                        int.TryParse(input, out int selectId);
+                        if (selectId > 0)
+                            posOnline.orderItem(selectId);
+                        else
+                            input = "Y";
+                        break;
+                }
+            } while (input != "N");
+
+            Console.WriteLine("Pos going off line");
+            Environment.Exit(0);
         }
     }
 }
